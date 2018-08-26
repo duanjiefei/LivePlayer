7ecfe338467d4132dcc4bfc4b21d745352df7535
duanjiefei <duanjiefei@skyworth.com>
2018-08-26 21:11:30 +0800

    利用DrawerLayout 为APP添加侧滑菜单

--- app/src/main/java/com/duanjiefei/github/liveplayer/HomeActivity.java
+++ app/src/main/java/com/duanjiefei/github/liveplayer/HomeActivity.java
@@ -2,10 +2,17 @@ package com.duanjiefei.github.liveplayer;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 
 import com.duanjiefei.github.liveplayer.base.BaseActivity;
 
 public class HomeActivity extends BaseActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private  ActionBarDrawerToggle mActionBarDrawerToggle;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +32,13 @@ public class HomeActivity extends BaseActivity {
         setSupportActionBar();
         setActionBarIcon(R.drawable.ic_drawer_home);
         setTitle("主页");
+
+        mDrawerLayout = bindViewID(R.id.drawLayout);
+        mNavigationView = bindViewID(R.id.navigation_view);
+
+
+        mActionBarDrawerToggle =  new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,R.string.drawer_open,R.string.drawer_close);
+        mActionBarDrawerToggle.syncState();
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
     }
 }
