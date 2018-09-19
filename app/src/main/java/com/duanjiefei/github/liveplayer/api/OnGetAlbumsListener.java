eb287280f45a0b7b2bc5c5f7a6d5ff9f9e8c14b4
duanjiefei <duanjiefei@skyworth.com>
2018-09-19 18:09:07 +0800

    添加直播节目 列表界面

--- app/src/main/java/com/duanjiefei/github/liveplayer/api/OnGetAlbumsListener.java
+++ app/src/main/java/com/duanjiefei/github/liveplayer/api/OnGetAlbumsListener.java
@@ -3,4 +3,8 @@ package com.duanjiefei.github.liveplayer.api;
 public interface OnGetAlbumsListener {
     //void OnGetChannelAlbumSuccess(AlbumList albumList);
     //void OnGetChannelAlbumFailed(ErrorInfo errorInfo);
+
+
 }
+
+
--- app/src/main/java/com/duanjiefei/github/liveplayer/live/LiveActivity.java
+++ app/src/main/java/com/duanjiefei/github/liveplayer/live/LiveActivity.java
@@ -2,26 +2,49 @@ package com.duanjiefei.github.liveplayer.live;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import com.duanjiefei.github.liveplayer.R;
 import com.duanjiefei.github.liveplayer.base.BaseActivity;
 
 public class LiveActivity  extends BaseActivity{
 
+    private RecyclerView recyclerView;
 
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_live;
+    }
     @Override
     protected void initData() {
 
     }
 
     @Override
-    protected void initView() {
-
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
-    protected int getLayoutID() {
-        return R.layout.activity_live;
+    protected void initView() {
+        setSupportActionBar();
+        setSupportArrowActionBar(true);
+        setTitle(R.string.channel_live);
+
+        recyclerView = bindViewID(R.id.ry_live);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,1);
+        gridLayoutManager.setOrientation(LinearLayout.VERTICAL);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        LiveItemAdapter liveAdapter = new LiveItemAdapter(this);
+        recyclerView.setAdapter(liveAdapter);
     }
 
     public static void launch(Activity activity){
